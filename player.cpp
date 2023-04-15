c8407ded001a3e4351d4e6f409028c8c51176015
Waahan <william@cryos.org>
2023-04-15 14:29:48 -0400

    Cleaning up the code

--- player.cpp
+++ player.cpp
@@ -3,8 +3,26 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 #include <string.h>
-#include "player.h"
+#include "headerPlayer.h"
+#include "headerVisuals.h"
 using namespace std;
+
+thing::thing(int ix, int iy, int iw, int ih, int ihealth, int ispeed, SDL_Texture* itexture)
+{
+    x = ix;
+    y = iy;
+    w = iw;
+    h = ih;
+    health = ihealth;
+    speed = ispeed;
+    texture = itexture;
+}
+
+user::user(int ix, int iy, int iw, int ih, int ihealth, int ispeed, SDL_Texture* itexture, int iback, int idirection) : thing(ix, iy, iw, ih, ihealth, ispeed, itexture)
+{
+	back = iback;
+	direction = idirection;
+}
 
 //For keyboard event detection. More scan codes at https://wiki.libsdl.org/SDL2/SDL_Scancode
 void doKeyDown(SDL_KeyboardEvent *event, int& playerUp, int& playerDown, int& playerLeft, int& playerRight, int& playerFired)
@@ -128,7 +146,7 @@ void doKeyUp(SDL_KeyboardEvent *event, int& playerUp, int& playerDown, int& play
 }
 
 
-void input(int& playerUp, int& playerDown, int& playerLeft, int& playerRight, int& playerFired)
+void input(user& player, thing& bullet, thing& bullet2, int& playerUp, int& playerDown, int& playerLeft, int& playerRight, int& playerFired)
 {
     SDL_Event event;
 
@@ -152,4 +170,220 @@ void input(int& playerUp, int& playerDown, int& playerLeft, int& playerRight, in
 		break;
 	}
     }
+    if (playerUp)
+    {
+            player.y -= player.speed;
+            player.direction = 1;
+    }
+    if (playerDown)
+    {
+            player.y += player.speed;
+            player.direction = 2;
+    }
+    if (playerLeft)
+    {
+            player.x -= player.speed;
+            player.direction = 3;
+    }
+    if (playerRight)
+    {
+            player.x += player.speed;
+            player.direction = 4;
+    }
+    if (playerFired && bullet.health == 0)
+    {
+            bullet.x = player.x;
+            bullet.y = player.y;
+            bullet.health = 1;
+            bullet.speed = player.speed/2;
+    }
+    else if (playerFired && bullet2.health == 0 && bullet.health == 1)
+    {
+            bullet2.x = player.x;
+            bullet2.y = player.y;
+            bullet2.health = 1;
+            bullet2.speed = player.speed/2;
+    }
+    else if(playerFired && bullet2.health > 0 && bullet.health > 0)
+    {
+            if(bullet.speed < 65535)
+            {
+                bullet.speed += bullet.speed;
+            }
+            else
+            {
+                bullet.speed = 20;
+            }
+            if(bullet2.speed < 65535)
+            {
+                bullet2.speed += bullet2.speed;
+            }
+            else
+            {
+                bullet2.speed = 20;
+            }
+    }
 }
+
+int noEscape(user player, int SCREEN_WIDTH, int SCREEN_HEIGHT)
+{
+    // This keeps the player on the screen, remember [x, y] where y is upside down
+    if (player.x < 0)
+    {
+	return 1;
+    }
+    else if (player.x > SCREEN_WIDTH-100)
+    {
+	return 2;
+    }
+    if (player.y < 0)
+    {
+	return 3;
+    }
+    else if (player.y > SCREEN_HEIGHT-100)
+    {
+	return 4;
+    }
+    else
+    {
+        return 0;
+    }
+}
+void noEscapeExec(user& player, int escape)
+{
+    if (escape == 1)
+    {
+        player.x += player.back;
+    }
+    else if (escape == 2)
+    {
+        player.x -= player.back;
+    }
+    else if (escape == 3)
+    {
+        player.y += player.back;
+    }
+    else if (escape == 4)
+    {
+        player.y -= player.back;
+    }
+}
+
+void bulletLogic(thing& bullet, user player, int SCREEN_WIDTH, int SCREEN_HEIGHT)
+{
+    if(player.direction == 1)
+    {
+        bullet.y -= bullet.speed;
+    }
+    else if(player.direction == 2)
+    {
+        bullet.y += bullet.speed;
+    }
+    else if(player.direction == 3)
+    {
+        bullet.x -= bullet.speed;
+    }
+    else if(player.direction == 4)
+    {
+        bullet.x += bullet.speed;
+    }
+
+    if (bullet.x > SCREEN_WIDTH)
+    {
+        bullet.health = 0;
+    }
+    else if (bullet.x < 0)
+    {
+        bullet.health = 0;
+    }
+    else if (bullet.y > SCREEN_HEIGHT)
+    {
+        bullet.health = 0;
+    }
+    else if (bullet.y < 0)
+    {
+        bullet.health = 0;
+    }
+}
+void thingLogic(thing& bullet, int SCREEN_WIDTH, int SCREEN_HEIGHT)
+{
+    if (bullet.x > SCREEN_WIDTH)
+    {
+        bullet.health = 0;
+	cout << "thing" << " is gone\n";
+    }
+    else if (bullet.x < 0)
+    {
+        bullet.health = 0;
+	cout << "thing" << " is gone\n";
+    }
+    else if (bullet.y > SCREEN_HEIGHT)
+    {
+        bullet.health = 0;
+	cout << "thing" << " is gone\n";
+    }
+    else if (bullet.y < 0)
+    {
+        bullet.health = 0;
+	cout << "thing" << " is gone\n";
+    }
+}
+
+void enemys(thing& enemy, int& enemySpawnTimer, App app)
+{
+    if(enemySpawnTimer <= 0 && enemy.health == 0)
+    {
+       cout << "Spawning enemy\n";
+
+       enemy.x = 1280;
+       enemy.y = rand() % 620;
+
+       enemy.speed = 1 + (rand() % 15);
+
+       if(enemy.speed == 0)
+       {
+           enemy.speed += 1;
+       }
+
+       enemy.health = 1;
+
+       enemy.x -= rand() % 10;
+
+       enemySpawnTimer = rand() % 100;
+    }
+    else
+    {
+        enemy.x -= enemy.speed;
+        app.imagePos(enemy.texture, enemy.x, enemy.y, enemy.w, enemy.h);
+
+        enemySpawnTimer--;
+    }
+}
+
+
+
+//Takes two objects dimetions
+int collision(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2)
+{
+        return (max(x1, x2) < min(x1 + w1, x2 + w2)) && (max(y1, y2) < min(y1 + h1, y2 + h2));
+}
+
+void didBulletHit(thing& bullet, thing& enemy)
+{
+    if(collision(bullet.x, bullet.y, bullet.w, bullet.h, enemy.x, enemy.y, enemy.w, enemy.h))
+    {
+        enemy.health = 0;
+        bullet.health -= 1;
+    }
+}
+
+void didEnemyKill(user& player, thing& enemy)
+{
+    if(collision(player.x, player.y, player.w, player.h, enemy.x, enemy.y, enemy.w, enemy.h))
+    {
+        enemy.health = 0;
+        player.health -= 1;
+        cout << "Health is:" << player.health << "\n";
+    }
+}
+
---
+++ visuals.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <algorithm>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "headerPlayer.h"
+using namespace std;
+
+App::App(int SCREEN_WIDTH, int SCREEN_HEIGHT)
+{
+    int rendererFlags, windowFlags;
+
+    rendererFlags = SDL_RENDERER_ACCELERATED;
+
+    windowFlags = 0;
+    
+    //Check if it is working
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        cout << "Could not start SDL:" << SDL_GetError();
+    }
+
+    window = SDL_CreateWindow("Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, windowFlags);
+
+    //Check if window was created
+    if (!window)
+    {
+        cout << "Failed to open window: " << SDL_GetError();
+    }
+
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+
+    //-1 so SDL use the first graphics acceleration device it finds
+    renderer = SDL_CreateRenderer(window, -1, rendererFlags);
+
+    //Check if renderer is working
+    if(!renderer)
+    {
+        cout << "Renderer failed: " << SDL_GetError();
+    }
+
+    //Allows window to load .png and .jpg images
+    // load support for the JPG and PNG image formats
+    int flags = IMG_INIT_JPG | IMG_INIT_PNG;
+    int initted = IMG_Init(flags);
+
+    if ((initted & flags) != flags) {
+        printf("IMG_Init: Failed to init required jpg and png support!\n");
+        printf("IMG_Init: %s\n", IMG_GetError());
+    }
+}
+
+
+//For loading image to the window
+SDL_Texture* App::loadImages(char* imageFile)
+{
+    //A variable to pass the image to SDL
+    SDL_Texture *Image;
+
+    SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO, "Loading %s", imageFile);
+
+    Image = IMG_LoadTexture(renderer, imageFile);
+
+    return Image;
+}
+
+//sets image location
+void App::imagePos(SDL_Texture* image, int x, int y, int w, int h)
+{
+    SDL_Rect dest;
+
+    //Where the image goes
+    dest.x = x;
+    dest.y = y;
+    dest.w = w;
+    dest.h = h;
+
+    //Query the attributes of a texture
+    //Takes image, Format(just set to NULL), Access(Also set to NULL), width and height
+    SDL_QueryTexture(image, NULL, NULL, &dest.w, &dest.h);
+
+    //Takes renderer, texture, NULL to copy whole image, &dest to know where to draw the image
+    SDL_RenderCopy(renderer, image, NULL, &dest);
+}
+
+void App::makeVisuals()
+{
+    //Color for background         0, 0, 0, 0, is for black
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    SDL_RenderClear(renderer);
+}
+
+void App::showVisuals()
+{
+    SDL_RenderPresent(renderer);
+}
+
