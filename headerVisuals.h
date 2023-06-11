f62774037a826e77d8b2aac6cdae04606d35df8d
Waahan <william@cryos.org>
2023-06-11 19:05:23 -0400

    Added to the README.md about the project Made the copy constructor delete in headerPlayer.h and headerVisuals.cpp Made messages colors const Added try blocks in constructors for visuals.cpp to free resources

--- headerVisuals.h
+++ headerVisuals.h
@@ -24,8 +24,10 @@ class App
     ~App();
 
     SDL_Texture *loadImages(const char* imageFile);
+    
     void imagePos(SDL_Texture* image, int x, int y, int w, int h);
     void imagePos(SDL_Texture* image, int x, int y);
+
     void makeVisuals();
     void showVisuals();
 
@@ -41,24 +43,26 @@ class Messages
     public:
     Messages(const char* message, int x, int y, int w, int h, App& app);
     Messages(const char* message, int x, int y, int w, int h, const SDL_Color& color, App& app);
+    Messages(Messages& copyMessage) = delete;
     ~Messages();
 
     void newMessage(const char* message, int x, int y, int w, int h, App& app);
     void newMessage(const char* message, int x, int y, int w, int h, const SDL_Color& color, App& app);
+    
     void drawMessage(App& app);
 
     private:
     TTF_Font* font = NULL;
     
     public:
-    SDL_Color White = {255, 255, 255};
-    SDL_Color Red = {255, 0, 0};
-    SDL_Color Orange = {255, 165, 0};
-    SDL_Color Yellow = {255, 255, 0};
-    SDL_Color Green = {0, 128, 0};
-    SDL_Color Blue = {0, 0, 255};
-    SDL_Color Indigo = {75, 0, 130};
-    SDL_Color Violet = {238, 130, 238};
+    const SDL_Color White = {255, 255, 255};
+    const SDL_Color Red = {255, 0, 0};
+    const SDL_Color Orange = {255, 165, 0};
+    const SDL_Color Yellow = {255, 255, 0};
+    const SDL_Color Green = {0, 128, 0};
+    const SDL_Color Blue = {0, 0, 255};
+    const SDL_Color Indigo = {75, 0, 130};
+    const SDL_Color Violet = {238, 130, 238};
 
     private:
     SDL_Surface* surfaceMessage = NULL;
