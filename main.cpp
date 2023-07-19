ffd3c89ae20b3376f11fdc7a4434eebac4f459ef
Waahan <william@cryos.org>
2023-07-19 18:23:20 -0400

    Removed unused climits header Removed App* constructor argument from thing Moved get functions to header file for thing Added setApp function Added static App* to thing Moved directions enum out of user class Changed doKeyDown to take a reference instead of a pointer Did the same to keyMenu Moved getDirection source to header file Made playerDeath private Made direction a directions enum Added animationCanCancel for future use Made all classes that do not have deconstructors have them set to be default Moved color enum out of Messages Updated main.cpp constructors and called thing::setApp Wrote some comments in player.cpp and visual.cpp for Pre and postconditions Added bullets.clear in ~user Made event handling functions use switch statements Updated playerDeath to not check health Updated makeEnd SDL_Delay from 60,000 to 3000 Used SDL_Rect generated constructor where needed

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <climits>
 #include <future>
 #include <memory>
 
@@ -22,18 +21,20 @@ int main(int argc, char* args[])
 
     App app(1280, 720);
 
-    user player(100, 100, 90, 90, 3, 10, "images/Player.png", &app, 30, 1);
+    thing::setApp(&app);
+    
+    user player(100, 100, 90, 90, 3, 10, "images/Player.png", 30);
 
-    enemys enemy(2000, 2000, 90, 90, 0, 0, "images/enemy.png", &app);
+    enemys enemy(2000, 2000, 90, 90, 0, 0, "images/enemy.png");
 
-    points point(2000, 2000, 40, 40, 0, 0, "images/points.png", &app);
+    points point(2000, 2000, 40, 40, 0, 0, "images/points.png");
 
-    thing background(0, 0, app.SCREEN_WIDTH, app.SCREEN_HEIGHT, 10, 0, "images/Background.png", &app);
+    thing background(0, 0, app.SCREEN_WIDTH, app.SCREEN_HEIGHT, 10, 0, "images/Background.png");
 
-    Messages Score("Score", 0, 0, 100, 100, &app, Messages::color::green);
+    Messages Score("Score", 0, 0, 100, 100, &app, color::green);
     Messages Title("Sus invaders", 0, 0, 500, 500, &app);
     Messages Start("Enter to start", app.SCREEN_WIDTH/2, app.SCREEN_HEIGHT/2, 500, 100, &app);
-    Messages Controls("W:up A:left S:down D:right SPACE:fire", 100, 500, 1000, 100, &app, Messages::color::red);
+    Messages Controls("W:up A:left S:down D:right SPACE:fire", 100, 500, 1000, 100, &app, color::red);
 
     while(!start)
     {
